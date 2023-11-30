93c37e9f15b8fa9bd2eca08d268b70b58d7f4a21
Pau Noguera <131747883+P4Unoguera@users.noreply.github.com>
2023-11-30 18:05:09 +0100

    commit

--- Lab5/Player.hpp
+++ Lab5/Player.hpp
@@ -1,4 +1,55 @@
 #ifndef _PLAYER_
 #define _PLAYER_
 
+#include "Country.hpp"
+class Match;
+
+class Player {
+
+protected:
+    bool female;
+    std::string name;
+    int age;
+    Country * country;
+    int numMatches;
+    
+public:
+    Player(bool f, std::string n, int a, Country * nac) {
+        female = f;
+        name = n;
+        country = nac;
+
+    }
+
+    bool isFemale() {
+        return female;
+
+    }
+
+    std::string getName() {
+        return name;
+
+    }
+
+    int getAge() {
+        return age;
+
+    }
+
+    Country * getNationality() {
+        return country;
+
+    }
+
+    int getNumMatches() {
+        return numMatches;
+
+    }
+    
+    virtual void updateStats(Match * m) = 0;
+
+    virtual void printStats() = 0;
+
+};
+
 #endif _PLAYER_
